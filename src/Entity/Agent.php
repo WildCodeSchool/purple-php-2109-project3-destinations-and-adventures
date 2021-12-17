92d51a224fd2963f31714780cbc684cdc32b1616
Alexandre Grué <alexandre.grue@hotmail.com>
2021-12-17 10:35:45 +0100

    creation of all entity necessary for the project

---
+++ src/Entity/Agent.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AgentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AgentRepository::class)
+ */
+class Agent
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $name;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $commission;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Booking::class, mappedBy="agent")
+     */
+    private Collection $bookings;
+
+    public function __construct()
+    {
+        $this->bookings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCommission(): ?float
+    {
+        return $this->commission;
+    }
+
+    public function setCommission(?float $commission): self
+    {
+        $this->commission = $commission;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Booking[]
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setAgent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->bookings->removeElement($booking)) {
+            // set the owning side to null (unless already changed)
+            if ($booking->getAgent() === $this) {
+                $booking->setAgent(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/SupplierPayment.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SupplierPaymentRepository;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SupplierPaymentRepository::class)
+ */
+class SupplierPayment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $dueAmount;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private ?DateTimeInterface $dueDate;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $exchangeRate;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $dueDollarsAmount;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private ?string $note;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private ?DateTimeInterface $date;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $paidAmount;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $dueCommission;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private ?DateTimeInterface $dueDateCommission;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Supplier::class, inversedBy="supplierPayments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?Supplier $supplier;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Booking::class, inversedBy="supplier_payment")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private ?Booking $booking;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDueAmount(): ?float
+    {
+        return $this->dueAmount;
+    }
+
+    public function setDueAmount(?float $dueAmount): self
+    {
+        $this->dueAmount = $dueAmount;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeInterface
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?\DateTimeInterface $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?float
+    {
+        return $this->exchangeRate;
+    }
+
+    public function setExchangeRate(?float $exchangeRate): self
+    {
+        $this->exchangeRate = $exchangeRate;
+
+        return $this;
+    }
+
+    public function getDueDollarsAmount(): ?float
+    {
+        return $this->dueDollarsAmount;
+    }
+
+    public function setDueDollarsAmount(?float $dueDollarsAmount): self
+    {
+        $this->dueDollarsAmount = $dueDollarsAmount;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getPaidAmount(): ?float
+    {
+        return $this->paidAmount;
+    }
+
+    public function setPaidAmount(?float $paidAmount): self
+    {
+        $this->paidAmount = $paidAmount;
+
+        return $this;
+    }
+
+    public function getDueCommission(): ?float
+    {
+        return $this->dueCommission;
+    }
+
+    public function setDueCommission(?float $dueCommission): self
+    {
+        $this->dueCommission = $dueCommission;
+
+        return $this;
+    }
+
+    public function getDueDateCommission(): ?\DateTimeInterface
+    {
+        return $this->dueDateCommission;
+    }
+
+    public function setDueDateCommission(?\DateTimeInterface $dueDateCommission): self
+    {
+        $this->dueDateCommission = $dueDateCommission;
+
+        return $this;
+    }
+
+    public function getSupplier(): ?Supplier
+    {
+        return $this->supplier;
+    }
+
+    public function setSupplier(?Supplier $supplier): self
+    {
+        $this->supplier = $supplier;
+
+        return $this;
+    }
+
+    public function getBooking(): ?Booking
+    {
+        return $this->booking;
+    }
+
+    public function setBooking(?Booking $booking): self
+    {
+        $this->booking = $booking;
+
+        return $this;
+    }
+}
