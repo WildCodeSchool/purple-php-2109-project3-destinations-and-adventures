14b59624a82c64424d3e36ca06d1ee9da54aca7e
Davy M <87863422+iStyyx@users.noreply.github.com>
2022-01-03 11:13:54 +0100

    Add / edit features of financial information

---
+++ src/Form/FinancialInfoType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FinancialInfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('total')
+            ->add('perPerson')
+            ->add('depositAmount')
+            ->add('dueDepositDate')
+            ->add('note')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
