eb97930f9bcce1ca36be122d5f371a7f3c58e0f8
Alexandre Grué <alexandre.grue@hotmail.com>
2022-01-05 12:24:25 +0100

    add use assert

--- src/Entity/SupplierPayment.php
+++ src/Entity/SupplierPayment.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\SupplierPaymentRepository;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SupplierPaymentRepository::class)
