68375cdddf46f8868e2a0b453b24c5727d15ca26
MaiwennBeloy <mcbeloy16@gmail.com>
2022-01-05 11:40:29 +0100

    changed fields's name and fields

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -8,22 +8,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\FormType;
-use App\Form\AddClientType;
+use App\Form\ClientType;
 use Doctrine\ORM\EntityManagerInterface;
 
 /**
- * @Route("/form", name="form_")
+ * @Route("/client", name="new_")
  */
 
 class ClientController extends AbstractController
 {
    /**
-    * @Route("/addClient", name="addClient")
+    * @Route("/client", name="new")
     */
     public function new(Request $request): Response
     {
         $client = new Client();
-        $form = $this->createForm(AddClientType::class, $client);
+        $form = $this->createForm(ClientType::class, $client);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
@@ -31,39 +31,23 @@ class ClientController extends AbstractController
             $entityManager->flush();
             return $this->redirectToRoute('clientPayment_edit');
         }
-        return $this->render('form/addClient.html.twig', ["form" => $form->createView()]);
+        return $this->render('client/new.html.twig', ["client" => $form->createView()]);
     }
 
-    /**
-     * @Route("/deleteClient", name="deleteClient")
-     */
-    public function delete(Request $request): Response
-    {
-        $delete = new Client();
-        $form = $this->createForm(AddClientType::class, $delete);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine() ->getManager();
-            $entityManager->persist($delete);
-            $entityManager->flush();
-            return $this->redirectToRoute('addClient');
-        }
-        return $this->render('form/deleteClient.html.twig', ["form" => $form->createView()]);
-    }
      /**
      * @Route("/modifyClient", name="modifyClient")
      */
     public function edit(Request $request): Response
     {
         $modify = new Client();
-        $form = $this->createForm(AddClientType::class, $modify);
+        $form = $this->createForm(ClientType::class, $modify);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine() ->getManager();
             $entityManager->persist($modify);
             $entityManager->flush();
-            return $this->redirectToRoute('addClient');
+            return $this->redirectToRoute('new');
         }
-        return $this->render('form/modifyClient.html.twig', ["form" => $form->createView()]);
+        return $this->render('client/modifyClient.html.twig', ["client" => $form->createView()]);
     }
 }
