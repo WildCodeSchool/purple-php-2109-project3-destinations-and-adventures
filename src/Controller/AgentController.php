be02f06dcd6eb535631efbb2d9424fd846185502
Soufiane <soufwra@gmail.com>
2022-01-11 16:10:00 +0100

    update the name of the main route

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/agent", name="name_")
+ * @Route("/agent", name="agent_")
  */
 class AgentController extends AbstractController
 {
