84425a99e02bbd3121161ed15289be56edd8fe53
Alexandre Grué <alexandre.grue@hotmail.com>
2022-01-12 11:11:58 +0100

    create edit function for client

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/booking/", name="client_")
+ */
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("client/{id}/edit", name="edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('client/edit.html.twig', [
+            'client' => $client,
+            'form' => $form,
+        ]);
+    }
+}
