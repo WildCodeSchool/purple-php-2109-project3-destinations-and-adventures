6a363adf6114cc3e7cc9e1cd8d7407b6e0525284
Alexandre Grué <alexandre.grue@hotmail.com>
2022-01-12 10:26:31 +0100

    resolce problem of cote

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/booking/", name="supplier_"")
+ * @Route("/booking/", name="supplier_")
  */
 class SupplierController extends AbstractController
 {
