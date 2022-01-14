486acadd4f327ee2eb98b5981537ab1a54b83d23
MaiwennBeloy <mcbeloy16@gmail.com>
2022-01-14 10:37:47 +0100

    conflicts fixed

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,34 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
 use App\Entity\Client;
+use App\Form\ClientType;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Form\FormType;
-use App\Form\ClientType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
- * @Route("/client", name="client_")
+ * @Route("/booking/", name="client_")
  */
-
 class ClientController extends AbstractController
 {
     /**
-     * @Route("/deleteClient", name="deleteClient")
+     * @Route("client/", name="index", methods={"GET"})
      */
-    public function delete(Request $request): Response
+    public function index(ClientRepository $clientRepository): Response
     {
-        $delete = new Client();
-        $form = $this->createForm(ClientType::class, $delete);
+        return $this->render('client/index.html.twig', [
+            'clients' => $clientRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("{booking_id}/client/new", name="new", methods={"GET", "POST"})
+     * @ParamConverter("booking", options={"mapping": {"booking_id": "id"}})
+     */
+    public function new(Request $request, Booking $booking, EntityManagerInterface $entityManager): Response
+    {
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine() ->getManager();
-            $entityManager->persist($delete);
+            $entityManager->persist($client);
             $entityManager->flush();
-            return $this->redirectToRoute('new');
+
+            return $this->redirectToRoute(
+                'client_payment_new',
+                ['booking_id' => $booking->getId()],
+                Response::HTTP_SEE_OTHER
+            );
         }
-        return $this->render('client/deleteClient.html.twig', ["client" => $form->createView()]);
+        return $this->renderForm('client/new.html.twig', [
+            'booking' => $booking,
+            'client' => $client,
+            'form' => $form,
+        ]);
     }
 }
