a2515a14e9ce8819ff9c2101f6cf5a396d3e1f20
Davy M <87863422+iStyyx@users.noreply.github.com>
2022-02-07 12:02:31 +0100

    CSS of Agents page

--- assets/app.js
+++ assets/app.js
@@ -19,6 +19,7 @@ import './styles/supplierPayment.scss';
 import './styles/supplierInformation.scss';
 import './styles/dashboard.scss';
 import './styles/accountCreation.scss';
+import './styles/agents.scss';
 
 // start the Stimulus application
 import './bootstrap';
