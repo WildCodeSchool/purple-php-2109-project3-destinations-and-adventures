98a628fedc64262da8a87f935ad37ad3e92a19c9
Alexandre Grué <alexandre.grue@hotmail.com>
2022-02-09 14:35:24 +0100

    auto completion in 2 direction with exchange rate

--- assets/js/supplierInfo.js
+++ assets/js/supplierInfo.js
@@ -6,3 +6,12 @@ document
         const exchangeRate = document.getElementById('supplier_information_exchangeRate').value;
         document.getElementById('supplier_information_dueDollarsAmount').value = Math.round(amount * exchangeRate);
     });
+
+document
+    .getElementById('supplier_information_dueDollarsAmount')
+    .addEventListener('blur', () => {
+        const amount = document
+            .getElementById('supplier_information_dueDollarsAmount').value;
+        const exchangeRate = document.getElementById('supplier_information_exchangeRate').value;
+        document.getElementById('supplier_information_dueAmount').value = Math.round(amount / exchangeRate);
+    });
